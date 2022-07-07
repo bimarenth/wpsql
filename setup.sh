d02a6ac3d176fe8460fc303763401d9b75b558ba
Bima Renthadinata <bimarnt29@gmail.com>
2022-07-07 13:26:55 +0700

    Update setup.sh
    Ganti variable

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 # Variables used in this script. Change anything with a value of <TODO>
-PROJECT_NAME=<TODO> # Your project name. You can see it in the URL of the portal i.e. https://console.cloud.google.com/run?project=my-cr-project&folder=&organizationId= then the project is "my-cr-project"
+PROJECT_NAME=Medicplus # Your project name. You can see it in the URL of the portal i.e. https://console.cloud.google.com/run?project=my-cr-project&folder=&organizationId= then the project is "my-cr-project"
 CLOUDSQL_INSTANCE=wp-mysql # This is the instance name - sorta like the MySQL install which can then have many databases in it. You're welcome to change it
 CLOUDSQL_TIER=db-f1-micro # I'm using the smallest MySQL instance. A full list is available here: https://cloud.google.com/sql/pricing#2nd-gen-pricing
 DBNAME=wordpress # The actual database. You're welcome to call it something different
 CLOUDSQL_TIER=db-f1-micro # I'm using the smallest MySQL instance. A full list is available here: https://cloud.google.com/sql/pricing#2nd-gen-pricing
-ROOT_PSWD=<TODO> # Enter a root password of your choice. Something strong!
-DB_USER=wordpress # You're welcome to change this if you like
-USER_PSWD=<TODO> # For the "wordpress" user, enter a password of your choice. Something strong!
-REGION=us-central1 # Feel free to set this to wahtever you like BUT be aware that, as of this writing, Cloud Run is not available everywhere. Full list of regions is here: https://cloud.google.com/compute/docs/regions-zones
-ZONE=us-central1-a # Once again, set it to whatever you like. See above link for the available zones
+ROOT_PSWD=inipassword # Enter a root password of your choice. Something strong!
+DB_USER=wpuser # You're welcome to change this if you like
+USER_PSWD=inipassword # For the "wordpress" user, enter a password of your choice. Something strong!
+REGION=asia-southeast2 # Feel free to set this to wahtever you like BUT be aware that, as of this writing, Cloud Run is not available everywhere. Full list of regions is here: https://cloud.google.com/compute/docs/regions-zones
+ZONE=asia-southeast2-a # Once again, set it to whatever you like. See above link for the available zones
 CLOUDSQL_INSTANCE_CONNECTION_NAME="$PROJECT_NAME:$REGION:$CLOUDSQL_INSTANCE" # The specific instance name we need later on
 
 # Setup basic config
