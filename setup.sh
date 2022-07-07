1bb6ea8fb6fe211c0a7a650e6c5c3e4da37a86c1
Bima Renthadinata <bimarnt29@gmail.com>
2022-07-07 13:32:12 +0700

    Update setup.sh
    Ganti lagi

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ gcloud config set compute/region $REGION
 gcloud config set compute/zone $ZONE
 
 # Create a minimal (micro) MySQL instance
-gcloud sql instances create $CLOUDSQL_INSTANCE --tier=$CLOUDSQL_TIER --region=us-central1
+gcloud sql instances create $CLOUDSQL_INSTANCE --tier=$CLOUDSQL_TIER --region=asia-southeast2
 gcloud sql users set-password root --host % --instance $CLOUDSQL_INSTANCE --password $ROOT_PSWD
 
 # Create a database called "wordpress" and create a user account
